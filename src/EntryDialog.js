59ede6b976ac3a5a031e276ef5fea06b74746637
yevgeniychaplygin3 <ychaplygin@yahoo.com>
2023-01-24 17:11:45 -0800

    working dialog

--- src/EntryDialog.js
+++ src/EntryDialog.js
@@ -5,6 +5,7 @@ class EntryDialog extends React.Component{
     constructor(props){
         super(props)
         this.state = {
+            empty: false
         }
     this.handleSubmit = this.handleSubmit.bind(this)
     }
@@ -13,8 +14,15 @@ class EntryDialog extends React.Component{
     
     handleSubmit(prop){
         // event.preventDefault()
-        console.log(prop)
-        alert(`${prop.currentState.state.caption} ${prop.currentState.state.url}`)
+        // console.log(prop)
+        // console.log(`${prop.currentState.state.caption} ${prop.currentState.state.url}`)
+        if(prop.currentState.state.caption == '' || prop.currentState.state.url == ''){
+            console.log('cannot be empty')
+            this.setState( (e) => e.empty = true)
+        }
+        else{
+            return (prop.currentState.closeDialog())
+        }
     }
 
 
@@ -23,9 +31,9 @@ class EntryDialog extends React.Component{
             <div className='dialog'>
                 <form onSubmit={(e) => {e.preventDefault(); this.handleSubmit(this.props)}}>
                     <h1>hello from the entry dialog</h1>
-                    <input placeholder="enter url" onChange={this.props.currentState.handleURLChange}></input>
+                    <input placeholder="enter url" onChange={this.props.currentState.handleURLChange} ></input>
                     <br/>
-                    <input placeholder="enter caption"  onChange={this.props.currentState.handleCaptionChange}></input>
+                    <input placeholder="enter caption"  onChange={this.props.currentState.handleCaptionChange} ></input>
                     <br/>
                     <button type='submit'>Accept</button>
                     <button onClick={(e) => {
@@ -34,6 +42,10 @@ class EntryDialog extends React.Component{
                         }}>
                             Cancel
                     </button>
+                    <div className=''>
+                        {/* {this.state.empty ? 'You must enter both a URL and a caption' : ''} */}
+                        {this.state.empty ? <p className='dialogError'>You must enter both a URL and a caption</p> : ''}
+                    </div>
                 </form>
 
                 {/* <button onClick={this.printfunc}>printfunc</button> */}
--- src/PhotoApp.js
+++ src/PhotoApp.js
@@ -48,12 +48,13 @@ class Dialog extends React.Component {
     render(){
         return(
             <>
-                <button onClick={this.openDialog}>
-                    Open Dialog Entry
-                </button>
-                    <div>
-                        {this.state.open ?  <EntryDialog currentState={this}/> : ''}
-                    </div>
+                {/* <button onClick={this.openDialog}> */}
+                    {/* Open Dialog Entry */}
+                {/* </button> */}
+                    {/* <div> */}
+                        
+                        {/* {this.state.open ?  <EntryDialog currentState={this}/> : ''} */}
+                    {/* </div> */}
             </>
         )
     }
@@ -62,15 +63,75 @@ class Dialog extends React.Component {
 
 
 
-function App(props){
-    return (
-        <div>
-            <Dialog/> 
-            <br/>
-            {/* <EntryDialog/> */}
-            <Pictures/>
-        </div>
-    )
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            dialogopen: false,
+            url: '', 
+            caption: ''
+        }
+        this.openDialog = this.openDialog.bind(this)
+        this.closeDialog = this.closeDialog.bind(this)
+        this.handleURLChange = this.handleURLChange.bind(this)
+        this.handleCaptionChange = this.handleCaptionChange.bind(this)
+
+
+    }
+
+    handleURLChange(event){
+        event.preventDefault()
+        this.setState({url: event.target.value})
+    }
+    handleCaptionChange(event){
+        event.preventDefault()
+        this.setState({caption: event.target.value})
+    }
+    
+
+    openDialog () {
+        // console.log(`open: ${this.state.dialogopen}`)
+        // this.setState(previousState => ({open: !previousState.open}))
+        this.setState((e) => e.open = true)
+    }
+
+    closeDialog(){
+        // console.log(`close: ${this.state.dialogopen}`)
+        this.setState((e) => e.open = false)
+        this.setState( (e) => e.url = '')
+        this.setState( (e) => e.caption = '')
+    }
+
+
+    
+    render(){
+        return (
+            <div>
+                <button onClick={this.openDialog}>
+                    Open Dialog Entry
+                </button>
+                <br/>
+                <div>
+                    {/* { 
+                        (
+                            () => 
+                            {
+                                if (this.state.dialogopen)
+                                {
+                                    return <EntryDialog currentState={this}/>
+                                }
+                            }  
+                        )
+                    } */}
+                    {this.state.open ?  <EntryDialog currentState={this}/> : '' }
+                    {/* console.log(`==caption ${this.state.caption}, ${this.state.url}`)} */}
+
+                </div>
+                {/* <EntryDialog/> */}
+                <Pictures/>
+            </div>
+        )
+    }
 }
 
 export default App
